7719cee0e8b1dfe2af87c0ff8cb417294e394727
artwilton <Arthur.M.Wilton@gmail.com>
2020-10-28 11:01:55 -0400

    list elements keep overwriting themselves

--- index.js
+++ index.js
@@ -3,4 +3,46 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
-});
+  // when a user presses the arrow keys, that direction (i.e. left, down, right, or up) is added to a list of directions on the right side of the robot's grid.
+
+  // event listener for arrow keys being pressed
+  // determine direction of arrow key
+  // add direction to <ul id="moves-container"> as <li> elements
+
+  
+  const leftText = document.createTextNode("Left")
+  const rightText = document.createTextNode("Right")
+  const upText = document.createTextNode("Up")
+  const downText = document.createTextNode("Down")
+
+  let list = document.createElement("LI")
+
+  document.body.addEventListener('keydown', function(event) {
+    switch (event.key) {
+      case "ArrowLeft":
+        list = document.createElement("LI")
+        list.appendChild(leftText)
+        console.log(list)
+        document.getElementById("moves-container").appendChild(list)
+        break;
+      case "ArrowRight":
+        list = document.createElement("LI")
+        list.appendChild(rightText)
+        console.log(list)
+        document.getElementById("moves-container").appendChild(list)
+        break;
+      case "ArrowUp":
+        list = document.createElement("LI")
+        list.appendChild(upText)
+        console.log(list)
+        document.getElementById("moves-container").appendChild(list)
+        break;
+      case "ArrowDown":
+        list = document.createElement("LI")
+        list.appendChild(downText)
+        console.log(list)
+        document.getElementById("moves-container").appendChild(list)
+        break;
+    }
+  })
+})
